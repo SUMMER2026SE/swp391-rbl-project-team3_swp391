12055620a45515db7e8b2560d499009c952a9079
Ncthangw <cuuthangnguyen1210@gmail.com>
2026-06-01 14:34:29 +0700

    Fix and Add HomePage

--- src/pages/jsx/LoginPage.jsx
+++ src/pages/jsx/LoginPage.jsx
@@ -7,6 +7,7 @@ function LoginPage({ switchToRegister }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
+    const [messageType, setMessageType] = useState("");
     const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
@@ -40,13 +41,19 @@ function LoginPage({ switchToRegister }) {
             }
 
             if (!response.ok) {
+<<<<<<< Updated upstream
                 setMessage(data.message || "Login Failed");
+=======
+                setMessage(data?.message || "Login Failed");
+                setMessageType("error");
+>>>>>>> Stashed changes
                 return;
             }
 
             localStorage.setItem("token", data.token);
 
             setMessage("✅ Login Successfully!");
+            setMessageType("success");
 
             setTimeout(() => {
                 navigate("/home");
@@ -80,6 +87,7 @@ function LoginPage({ switchToRegister }) {
 
             if (!res.ok) {
                 setMessage(data.message || "Google Login Failed");
+                setMessageType("error");
                 return;
             }
 
@@ -87,6 +95,7 @@ function LoginPage({ switchToRegister }) {
             localStorage.setItem("user", JSON.stringify(data.user));
 
             setMessage("✅ Google Login Success!");
+            setMessageType("success");
 
             navigate("/home");
 
@@ -145,7 +154,7 @@ function LoginPage({ switchToRegister }) {
                     </div>
 
                     {message && (
-                        <div className="message-box">
+                        <div className={`message-box ${messageType}`}>
                             {message}
                         </div>
                     )}
